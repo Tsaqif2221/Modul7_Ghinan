c4e94abd57349c3486ef8d7c43d70c43c44a3d3b
Tsaqif2221 <afifashiilah@gmail.com>
2026-04-18 14:30:39 +0700

    Menambahkan class FilmFavorit

---
+++ Modul7_Ghinan/FilmFavorit_103022430004.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_Ghinan
+{
+
+    public class FilmFavorit_103022430004
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public double rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("jurnal7_1_103022430004.json");
+            var data = JsonSerializer.Deserialize<FilmFavorit_103022430004>(jsonString);
+            Console.WriteLine($"Title : {data.title}");
+            Console.WriteLine($"Director : {data.director}");
+            Console.WriteLine($"Year : {data.year}");
+            Console.WriteLine($"Genre : {data.genre}");
+            Console.WriteLine($"Rating : {data.rating}");
+            Console.WriteLine($"Duration : {data.durationMinutes}");
+            Console.WriteLine($"IsWatched : {data.isWatched}");
+        }
+    }
+}
--- Modul7_Ghinan/Program.cs
+++ Modul7_Ghinan/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Modul7_Ghinan;
+using System;
+class Program_tsaqif
+{
+    static void Main(string[] args)
+    {
+        var Filmfavorit_103022430004 = new FilmFavorit_103022430004();
+        Filmfavorit_103022430004.ReadJSON();
+    }
+}
